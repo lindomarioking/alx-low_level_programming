28a14fa6b3532972dd703040e49ca173e94fa847
Ifeanyi Philip <lindomarioking90@gmail.com>
2022-06-30 16:37:12 -0700

    C programming project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry ponit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like buiding a multilingual pussle");
+	return (0);
+}
