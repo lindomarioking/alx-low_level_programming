546f7f14df129b638c0b50c2045471c7fa43970b
Ifeanyi Philip <lindomarioking90@gmail.com>
2022-06-30 16:52:31 -0700

    Fixing output error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry ponit
- * Return: Always 0 (Success)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like buiding a multilingual pussle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
